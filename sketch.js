d7f36584bd4f53ad666efec88a4f5ea75244e4cb
René Martínez <hola@r3n0.com>
2023-01-24 11:43:10 -0500

    hice unos cambios

--- sketch.js
+++ sketch.js
@@ -1,83 +1,97 @@
-
 let gota = [];
-const nGotas = 200;//Cantidad de gotas
+const nGotas = 500; //Cantidad de gotas
 let newElem;
+let diam = 8;
+
+let letras = 'abcdefghijklmnopqrstuvwxyz1234567890/ABCDEFGHIHKLMNOPQRSTUVWXZY';
 
 function setup() {
-  createCanvas(1080 , 720);
-  for (let i = 0; i < nGotas; i++) {
-    newElem = new drop(width, height);
-    gota.push(newElem);
-  }  
+	createCanvas(1080, 720);
+	// frameRate(20);
+	for (let i = 0; i < nGotas; i++) {
+		newElem = new drop(width, height);
+		gota.push(newElem);
+	}
 }
 
 function draw() {
-  background(0);
-  for (let i = 0; i < gota.length; i++) {
-    gota[i].update();
-    gota[i].display();
-  }  
+	background(0);
+	for (let i = 0; i < gota.length; i++) {
+		gota[i].update();
+		gota[i].display();
+	}
 }
 
-class drop{
-  constructor (_x , _y){//Crea los parametros para las gotas
+class drop {
+	constructor(_x, _y) {
+		//Crea los parametros para las gotas
 
-    this.x = random(_x);//posicion en x
-    this.y = random(-50, -250);//posicion en y
+		this.x = random(_x); //posicion en x
+		this.y = random(-50, -250); //posicion en y
 
-    this.ySpeed = random(1,3);//velocidad de la gota
+		this.restePos();
+	}
 
-    //this.len = random (10,20);//longitud de la gota (solo para las lineas)
+	update() {
+		//Actualiza la posicion de la gota
+		if (frameCount % this.ySpeed == 0) {
+			this.y += diam;
+		}
 
-  }
-  
-  update(){ //Actualiza la posicion de la gota
-    this.y = this.y + this.ySpeed;
-    if (this.y > width) {//Asigna otros valores al llegar al final de la pantalla
-      this.y = random(-50 , -200); //Rango de aparicion de las gotas
-      this.ySpeed = random(1,3); //Velocidad de caida
-    }
-  }
+		if (this.y > width) {
+			//Asigna otros valores al llegar al final de la pantalla
+			this.restePos();
+		}
+	}
 
-  display(){//Muestra la gota
+	restePos() {
+		this.ySpeed = floor(random(10)) * diam; //velocidad de la gota
+		this.longitud = round(random(2, 30));
+		this.y = random(-50, -250);
+	}
 
-    //----Lineas----
-    /*
+	display() {
+		//Muestra la gota
+
+		//----Lineas----
+		/*
     stroke (120,250,80);
     strokeWeight(4);
     line (this.x, this.y, this.x, this.y+this.len);
     */
 
-    //----Circulos----
+		//----Circulos----
 
-    let siz = 8; //Tamaño del circulo
-    let col = color(120,250,80);
-    
-    for (let i = 0; i < int (random(4 , 6)) ; i++) {      
-      col.setAlpha(255 - (i * 40));
-      fill(col);
-      noStroke();
-      circle (this.x, this.y - ( siz * i), 8);      
-    }
-    
-    
-    // var n = 0;
-    // if (n <= random(4,6)) {
-    //   circle (this.x, this.y + (n * 8), 8);
-    //   n++;
-    // }
+		//Tamaño del circulo
+		let col = color(120, 250, 80);
 
-    // let col = color(120,250,80);
-    // col.setAlpha(120);
-    // noStroke();
-    // fill (col);
-    // circle (this.x, this.y, 8);
-    // circle (this.x, this.y+8, 8);
-    // circle (this.x, this.y+16, 8);
-    // circle (this.x, this.y+24, 8);
-    
-    
-  }
+		for (let i = 0; i < this.longitud; i++) {
+			let alpha = map(i, 0, this.longitud, 255, 10);
+			col.setAlpha(alpha);
+			fill(col);
+			noStroke();
+			let letraRandom = floor(random(letras.length));
+			text(
+				letras.slice(letraRandom, letraRandom + 1),
+				this.x,
+				this.y - diam * i
+			);
+			// circle(this.x, thiss.y - diam * i, diam);
+		}
+
+		// var n = 0;
+		// if (n <= random(4,6)) {
+		//   circle (this.x, this.y + (n * 8), 8);
+		//   n++;
+		// }
+
+		// let col = color(120,250,80);
+		// col.setAlpha(120);
+		// noStroke();
+		// fill (col);
+		// circle (this.x, this.y, 8);
+		// circle (this.x, this.y+8, 8);
+		// circle (this.x, this.y+16, 8);
+		// circle (this.x, this.y+24, 8);
+	}
 }
-
-
